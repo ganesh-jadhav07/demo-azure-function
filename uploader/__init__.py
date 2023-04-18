6827e00858ab2e02ddaec66f5f1170d1f2d7f3ea
ganesh-jadhav07 <112162853+ganesh-jadhav07@users.noreply.github.com>
2023-04-18 19:10:11 +0530

    Update __init__.py

--- uploader/__init__.py
+++ uploader/__init__.py
@@ -7,15 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     req_body_data = req.get_body()
     if req_body_data:
-        try:
-          mgu_id = req_body_data.get('mgu_id')
-      
-        except ValueError:
-            pass
-
-    if mgu_id:
-#         return_value = upload_to_blob(mgu_id,req_file)
-        return func.HttpResponse(mgu_id)
+        return func.HttpResponse(req_body_data)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Please upload a file",
